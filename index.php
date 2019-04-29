0b8dc6432fcc9e2ae1bbf11d469f0c3173d76a32
Reynaldo <reynaldo119@gmail.com>
2019-04-29 12:37:47 -0500

    new

--- index.php
+++ index.php
@@ -5,9 +5,53 @@
     <title>Parser</title>
   </head>
   <body>
-    <h1>HOLA</h1>
     <?php
     $archivo = fopen("Facturas.txt","r") or die ("Unable to read the file");
+
+    class Header
+    {
+      public $noFactura;
+      public $noCliente;
+      public $fecha;
+      public $moneda;
+
+      function __construct($noFactura,$noCliente,$fecha,$moneda)
+      {
+        $this->noFactura = $noFactura;
+        $this->noCliente = $noCliente;
+        $this->fecha = $fecha;
+        $this->moneda = $moneda;
+      }
+    }
+
+    class Item
+    {
+      public $idProd;
+      public $antiguedad;
+      public $cantidad;
+      public $valor;
+
+      function __construct($idProd,$antiguedad,$cantidad,$valor)
+      {
+        $this->idProd = $idProd;
+        $this->antiguedad = $antiguedad;
+        $this->cantidad = $cantidad;
+        $this->valor = $valor;
+      }
+    }
+
+    class Trailer
+    {
+      public $totalLineas;
+      public $total;
+
+
+      function __construct($totalLineas,$total)
+      {
+        $this->totalLineas = $totalLineas;
+        $this->total = $total;
+      }
+    }
 
     $info = array();
 
@@ -19,9 +63,45 @@
 
     foreach ($info as $value) {
       $char = str_split($value);
-      echo sizeof($char)."</br>";
-      echo $value."</br>";
+      switch ($char[0]) {
+        case 'H':
+        echo "</br>Header</br>";
+          $noFactura = array_slice($char,1,8);
+          $noFactura = implode($noFactura);
+          $noCliente = array_slice($char,9,6);
+          $noCliente = implode($noCliente);
+          $fecha = array_slice($char,-18,8);
+          $fecha = implode($fecha);
+          $moneda = array_slice($char,-10,3);
+          $moneda = implode($moneda);
+          echo "</br>".$noFactura."</br>".$noCliente."</br>".$fecha."</br>".$moneda."</br></br>";
+          //$header = new Header();
+          break;
+        case 'I':
+          echo "<br>Item</br>";
+          $idProd = array_slice($char,1,8);
+          $idProd = implode($idProd);
+          $antiguedad = array_slice($char,9,1);
+          $antiguedad = implode($antiguedad);
+          $cantidad = array_slice($char,12,4);
+          $cantidad = implode($cantidad);
+          $valor = array_slice($char,17,8);
+          $valor = implode($valor);
+          $valor = trim($valor);
+          echo "</br>".$idProd."</br>".$antiguedad."</br>".$cantidad."</br>".$valor."</br></br>";
+          var_dump($valor)."";
+          break;
+        case 'T':
+          echo "<br>Trailer";
+          break;
+
+        default:
+          echo "<br>Cosas extraños <br>";
+          break;
+      }
     }
+
+
 
    ?>
   </body>
